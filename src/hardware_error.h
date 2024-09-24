59877cd5b2fb6e381980a6a317409c147f14f0cd
AlanCui4080 <me@alancui.cc>
2024-09-25 04:31:19 +0800

    fix: add LR to the end of hal_perror

--- src/hardware_error.h
+++ src/hardware_error.h
@@ -1,3 +1,3 @@
 #include <stdio.h>
 #include <stm32g4xx_hal.h>
-#define hal_perror(m, x, r) printf(m ": failed to " x " error: %u at %s:%d", (unsigned int)r, __FILE__, __LINE__)
+#define hal_perror(m, x, r) printf(m ": failed to " x " error: %u at %s:%d\n", (unsigned int)r, __FILE__, __LINE__)
